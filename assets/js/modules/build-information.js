a2d28a6afdffdcd2b8a1e502f8255629a580704d
PedrozaContadores <marcos.henrique@pedroza.com.br>
2026-07-25 03:03:38 -0300

    Sprint 1.1.4 - refinamento visual final

--- assets/js/modules/build-information.js
+++ assets/js/modules/build-information.js
@@ -1,7 +1,6 @@
 export const BUILD_INFORMATION = Object.freeze({
   project: "Portal Pedroza Contadores",
   sprint: "1.1",
-  version: "1.1.0",
-  environment: "production",
-  branch: "main"
+  version: "1.1.4",
+  status: "Homologação visual"
 });
